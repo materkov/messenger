b0da963dfa1050529fe747de5ad22858c0b53301
Maks <materkov@gmail.com>
2018-08-14 23:21:01 +0300

    .env

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import newrelic.agent
+
 newrelic.agent.initialize()
-
-
 from flask import Flask
+import os
+
+with open('.env') as f:
+    lines = f.readlines()
+    for line in lines:
+        k, v = line.split('=', 2)
+        k = k.strip()
+        v = v.strip()
+        os.environ[k] = v
+
 app = Flask(__name__)
 
 import api
